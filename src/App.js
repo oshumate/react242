ae9e0ab637d18b2e41c221ce443c817ea2bceb5a
Owen Shumate <oshumate@email.sc.edu>
2025-03-28 13:35:36 -0400

    project8

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+// import other pages as needed, e.g. Menus, Reservations, Order, etc.
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Future routes:
+        <Route path="/menus" element={<Menus />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/order" element={<Order />} />
+        */}
+      </Routes>
+    </Router>
   );
 }
 
