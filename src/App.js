05a495bff2921e2b27c8c2b7f303aa6ac75399f4
Owen Shumate <oshumate@email.sc.edu>
2025-03-30 22:02:14 -0400

    project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Menus from './pages/Menus';
 import Reservations from './pages/Reservations';
